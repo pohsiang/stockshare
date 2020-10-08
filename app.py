6a69364102a541272ca859088976b4fe230e66dd
phwang <pohsiang.wang@oneplus.com>
2020-10-08 17:39:10 +0800

    Fix some bug when getting information from flask

--- app.py
+++ app.py
@@ -13,22 +13,26 @@ class ChangingStackHolder(Resource):
 
     def get(self):
         cursor = self.db.get_changing_stockholder_result()
-        result = []
+        result = {}
+        data = []
         for item in cursor:
-            result.append(item['stockid'])
+            data.append(item['stockid'])
+        result['stocklist'] = data
         return result
 
 class StackHodlerDetail(Resource):
-    
+
     def __init__(self):
         self.db = PHStockDB()
-    
+
     def get(self, stockid):
         cursor = self.db.get_single_stock_stockholder(stockid)
         result = {}
         for item in cursor:
             result = item
-        result.pop("_id")
+
+        result.pop('_id')
+        print(result)
         return result
 
 api.add_resource(ChangingStackHolder, '/changing')
